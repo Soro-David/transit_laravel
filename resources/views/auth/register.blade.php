c98766707de4e3be843fa1bb16a497f8b236aa49
ddavid <soroddavid63@gmail.com>
2025-02-20 12:07:52 +0000

    1er essai de camera

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -129,7 +129,7 @@
 @section('css')
 <style>
    body {
-    background-image: url('{{ asset('images/font_login.jpg') }}'); /* Chemin de l'image de fond */
+    background-image: url('{{ asset('images/login2.png') }}'); /* Chemin de l'image de fond */
     background-size: auto; /* L'image garde sa taille d'origine */
     background-position: center; /* L'image reste centrée */
     background-repeat: no-repeat; /* L'image ne se répète pas */
