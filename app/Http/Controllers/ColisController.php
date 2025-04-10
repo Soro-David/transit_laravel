5ee2e443457ead14f568df62dedb89ae3555ee00
ddavid <soroddavid63@gmail.com>
2025-04-10 11:14:27 +0000

    le A de app

--- app/Http/Controllers/ColisController.php
+++ app/Http/Controllers/ColisController.php
@@ -35,7 +35,7 @@ use Infobip\Configuration;
 use Infobip\Models\SmsAdvancedTextualRequest;
 use Infobip\Models\SmsDestination;
 use Infobip\Models\SmsTextualMessage;
-use App\Services\infobipService;
+use App\Services\InfobipService;
 
 
 class ColisController extends Controller
--- app/Services/infobipservice.php
+++ app/Services/infobipservice.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 
-class infobipService
+class InfobipService
 {
     protected $apiKey;
     protected $baseUrl;
