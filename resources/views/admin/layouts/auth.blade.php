b976320647bf83be5a1c815f29f19f73a0684034
ddavid <soroddavid63@gmail.com>
2025-06-16 08:22:18 +0000

    font-awesome

--- resources/views/admin/layouts/auth.blade.php
+++ resources/views/admin/layouts/auth.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     @yield('css')
 </head>
 
