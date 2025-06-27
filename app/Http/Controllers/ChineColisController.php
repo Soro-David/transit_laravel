87dd37c76ed95d3cf1145ca151595a70270b63c1
Soro-David <soroddavid63@gmail.com>
2025-06-27 11:54:09 +0000

    dd(data)

--- app/Http/Controllers/ChineColisController.php
+++ app/Http/Controllers/ChineColisController.php
@@ -669,7 +669,7 @@ public function vol_fermer(Request $request)
                     $colisEnregistres[] = $colisModel->fresh();
                 }
             }
-            // dd($colisEnregistres);
+            dd($colisEnregistres);
             // 5. Mise à jour finale et validation de la transaction
             if (empty($colisEnregistres)) {
                 throw new \Exception("Aucun colis n'a été créé, annulation de la transaction.");
