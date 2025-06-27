85e09eca23eb7e1900f70d956d2c0238a0f1a2c9
Soro-David <soroddavid63@gmail.com>
2025-06-27 09:21:42 +0000

    resoulu 1

--- resources/views/admin/colis/add_colis.blade.php
+++ resources/views/admin/colis/add_colis.blade.php
@@ -640,7 +640,6 @@ $(document).ready(function() {
                 agence: agence,
                 prix: prix,
             }),
-            console.log(data);
             success: function (response) {
                 alert(response.message); // Affichage du message de succès
                 $("#produitForm")[0].reset(); // Réinitialisation du formulaire
