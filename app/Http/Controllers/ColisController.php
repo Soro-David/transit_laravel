eb0db7b5380e39104a582a6f33c4e63db225d88f
Soro-David <soroddavid63@gmail.com>
2025-09-02 16:16:52 +0000

    agence de destination bateau seule

--- app/Http/Controllers/ColisController.php
+++ app/Http/Controllers/ColisController.php
@@ -851,7 +851,7 @@ class ColisController extends Controller
             } else {
                 $referenceColisPrincipale = 'REF-' . strtoupper(uniqid()); // Fallback
             }
-            dd($referenceColisPrincipale);
+            // dd($referenceColisPrincipale);
         // --- CORRECTION 1 : Création du paiement principal en amont ---
         // Cet enregistrement représente la transaction globale.
         $paiementPrincipal = null;
