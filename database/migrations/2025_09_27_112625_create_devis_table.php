10e4ed8ff942152c9e84d2f8319711198020e44b
Soro-David <soroddavid63@gmail.com>
2025-10-06 09:31:30 +0000

    problem de recuperation

--- database/migrations/2025_09_27_112625_create_devis_table.php
+++ database/migrations/2025_09_27_112625_create_devis_table.php
@@ -16,19 +16,17 @@ return new class extends Migration
         Schema::create('devis', function (Blueprint $table) {
             $table->id();
             $table->string('reference');
-            $table->string('mode_transit'); // 'maritime' ou 'aerien'
+            $table->string('mode_transit');
             $table->string('pays_expedition');
             $table->string('agence_expedition');
-            $table->string('agence_destination'); // Nom unifié pour la destination
-            // --- Informations sur l'expéditeur (le client) ---
+            $table->string('agence_destination');
             $table->string('nom_expediteur');
             $table->string('prenom_expediteur');
             $table->string('email_expediteur')->nullable();
             $table->string('tel_expediteur')->nullable();
             $table->text('adresse_expediteur');
-            // --- Information financière globale ---
             $table->string('devise', 10); 
-            $table->decimal('montant', 10, 2)->nullable(); // <-- AJOUTEZ ->nullable()
+            $table->decimal('montant', 10, 2)->nullable();
             $table->string('etat')->default('Devis');
             $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
             $table->foreignId('chauffeur_id')->nullable()->constrained()->onDelete('set null');
