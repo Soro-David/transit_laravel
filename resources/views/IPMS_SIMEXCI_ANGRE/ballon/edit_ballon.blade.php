a6c20884df8356f71f45851dc193da987fb591f3
Soro-David <soroddavid63@gmail.com>
2025-10-15 13:07:18 +0000

    affichage des colis dans vol

--- resources/views/IPMS_SIMEXCI_ANGRE/ballon/edit_ballon.blade.php
+++ resources/views/IPMS_SIMEXCI_ANGRE/ballon/edit_ballon.blade.php
@@ -39,6 +39,7 @@
     </div>
 </section>
 @endsection
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
